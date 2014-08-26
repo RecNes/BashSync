d63e40833e503163e2e4d5e3d2d2713961724184
Sencer H. <recnes@recnes.com>
2014-08-26 14:19:58 +0300

    Create bashsync.sh

---
+++ bashsync.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+RSYNC=/usr/bin/rsync
+SSH=/usr/bin/ssh
+KEY=/home/user/.ssh/id_rsa.pub
+RUSER=user_name
+RHOST=example.com
+RPATH=/home/user/remote_path_of_files/
+LPATH=/home/user/local_path_of_backup_dir/
+LOG=/home/user/local_path_of_backup_dir/transfer.log
+PIDS=`pidof rsync`
+
+if [ -z "$PIDS" ]; then
+        echo "No process alive... Starting an instance."
+        $RSYNC --partial --progress -avvz -e $SSH $RHOST:$RPATH $LPATH 2>&1 | tee $LOG
+        exit 1
+else
+        echo "An instace is running in background. Done nothing."
+fi
+
